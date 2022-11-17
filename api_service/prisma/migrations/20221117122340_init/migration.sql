6133a6323b3546ed5f1f4a489f574ceb1bd9c1aa
Radomir Djokovic <radomir.djokovic@yara.com>
2022-11-17 13:28:56 +0100

    Change DB to PostgreSQL + add Swagger

--- api_service/prisma/migrations/20221117122340_init/migration.sql
+++ api_service/prisma/migrations/20221117122340_init/migration.sql
@@ -1,21 +1,27 @@
 -- CreateTable
 CREATE TABLE "User" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
-    "admin" BOOLEAN NOT NULL
+    "admin" BOOLEAN NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Report" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "userId" TEXT,
     "price" INTEGER NOT NULL,
     "approved" BOOLEAN NOT NULL,
     "make" TEXT NOT NULL,
     "model" TEXT NOT NULL,
-    CONSTRAINT "Report_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+
+    CONSTRAINT "Report_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Report" ADD CONSTRAINT "Report_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
