081905295b9bcc8f6431fa2c0cff74fa8bd90aae
Radomir Djokovic <radomir.djokovic@yara.com>
2022-11-19 18:27:32 +0100

    Pritify changes

--- api_service/src/reports/reports.service.ts
+++ api_service/src/reports/reports.service.ts
@@ -7,53 +7,50 @@ import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class ReportsService {
-    constructor(private prisma: PrismaService) {
-    }
+  constructor(private prisma: PrismaService) {}
 
-    create(reportDto: CreateReportDto, user: User) {
-        return this.prisma.report.create({
-            data: {
-                ...reportDto,
-                approved: false,
-                user: {
-                    connect: {
-                    id: user.id
-                } }
-            },
-            include: {
-                user: true
-            }
-        });
-    }
+  create(reportDto: CreateReportDto, user: User) {
+    return this.prisma.report.create({
+      data: {
+        ...reportDto,
+        approved: false,
+        user: {
+          connect: {
+            id: user.id,
+          },
+        },
+      },
+      include: {
+        user: true,
+      },
+    });
+  }
 
-    async changeApproval(id: string, approved: boolean) {
-        const report = await this.prisma.report.findUnique({
-            where: { id }
-        });      
-        if(!report) throw new NotFoundException('Report Not Found');
+  async changeApproval(id: string, approved: boolean) {
+    const report = await this.prisma.report.findUnique({
+      where: { id },
+    });
+    if (!report) throw new NotFoundException('Report Not Found');
 
-        return this.prisma.report.update(
-            { where: { id },
-            data: { approved }
-        })
-    }
+    return this.prisma.report.update({ where: { id }, data: { approved } });
+  }
 
-    async createEstimate({ make, model }: GetEstimateDto) {
-        const rsp = await this.prisma.report.aggregate({
-            _avg: {
-                price: true,
-            },
-            where: {
-                make,
-                model,
-                approved: true
-            },
-            orderBy: {
-                price: 'asc'
-            },
-            take: 3
-        });
+  async createEstimate({ make, model }: GetEstimateDto) {
+    const rsp = await this.prisma.report.aggregate({
+      _avg: {
+        price: true,
+      },
+      where: {
+        make,
+        model,
+        approved: true,
+      },
+      orderBy: {
+        price: 'asc',
+      },
+      take: 3,
+    });
 
-        return rsp._avg.price;
-    }
+    return rsp._avg.price;
+  }
 }
--- api_service/src/users/auth.service.ts
+++ api_service/src/users/auth.service.ts
@@ -1,36 +1,37 @@
-import { Injectable, NotFoundException, BadRequestException } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  BadRequestException,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { randomBytes, scrypt as _scrypt } from 'crypto';
 import { promisify } from 'util';
 
 const scrypt = promisify(_scrypt);
 
-
 @Injectable()
 export class AuthService {
-    constructor(private usersService: UsersService) {
+  constructor(private usersService: UsersService) {}
+
+  async signup(email: string, password: string) {
+    const userList = await this.usersService.find(email);
+    if (userList.length) throw new BadRequestException('Email in use');
+
+    const salt = randomBytes(8).toString('hex');
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+    const result = salt + '.' + hash.toString('hex');
+
+    return this.usersService.createUser(email, result);
+  }
+
+  async signin(email: string, password: string) {
+    const [user] = await this.usersService.find(email);
+    if (!user) throw new NotFoundException('User Not Found');
+    const [salt, storedHash] = user.password.split('.');
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+    if (storedHash !== hash.toString('hex')) {
+      throw new BadRequestException('Bad Password');
     }
-
-    async signup(email: string, password: string) {
-        const userList = await this.usersService.find(email);
-        if(userList.length) throw new BadRequestException('Email in use');
-
-        const salt = randomBytes(8).toString('hex');
-        const hash = await scrypt(password, salt, 32) as Buffer;
-        const result = salt + '.' + hash.toString('hex');
-
-
-        return this.usersService.createUser(email, result);
-    }
-
-    async signin(email: string, password: string) {
-        const [user] = await this.usersService.find(email);
-        if(!user) throw new NotFoundException('User Not Found');
-        const [salt, storedHash] = user.password.split('.');
-        const hash = await scrypt(password, salt, 32) as Buffer;
-        if(storedHash !== hash.toString('hex')) {
-            throw new BadRequestException('Bad Password')
-        }
-        return user
-    }
+    return user;
+  }
 }
