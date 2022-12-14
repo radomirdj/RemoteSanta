1d2f326ba0b74f2b903cb9a74ff9018d235d89b2
Radomir Djokovic <radomir.djokovic@yara.com>
2022-12-14 08:01:16 +0100

    Fix in private route

--- client_app/src/app/components/privateRoute/PrivateRoute.tsx
+++ client_app/src/app/components/privateRoute/PrivateRoute.tsx
@@ -1,13 +1,9 @@
 import { Navigate, Route } from "react-router-dom";
-import { useSelector } from "react-redux";
 import { ReactNode } from "react";
 
-import { getAuthUserSelector } from "../../store/auth/selectors";
-
 function PrivateRoute({ children }: { children: ReactNode }) {
-  console.log("PrivateRoute -> children", children);
-  const user = useSelector(getAuthUserSelector);
-  if (!user.accessToken) {
+  const token = localStorage.getItem("token");
+  if (!token) {
     return <Navigate to="/login" replace />;
   }
   return <div>{children}</div>;
