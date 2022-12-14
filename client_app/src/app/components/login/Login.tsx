8f8b80c4cd14c037ab5a9f4a7145984086b356fc
jovanadjekovic <djekovicjovana1@gmail.com>
2022-12-14 16:50:38 +0100

    public header

--- client_app/src/app/components/login/Login.tsx
+++ client_app/src/app/components/login/Login.tsx
@@ -1,9 +1,8 @@
 import React, { useState } from "react";
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
 import { loginRequest } from "../../store/auth/actions";
 import { useDispatch, useSelector } from "react-redux";
 import { getErrorSelector } from "../../store/auth/selectors";
+import AppHeaderPublic from "../app-header-public/AppHeaderPublic";
 
 const Login = () => {
     const dispatch = useDispatch();
@@ -15,14 +14,21 @@ const Login = () => {
         dispatch(loginRequest({ email, password }));
     }
 
+
+
     return (
-        <div className="login">
-            {error && <b className="error">{error}</b>}
-            <br />
-            <TextField id="outlined-basic" label="Email" variant="outlined" value={email} onChange={(e) => setEmail(e.target.value)} />
-            <TextField id="outlined-basic" label="Password" variant="outlined" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-            <Button variant="contained" onClick={login}>Login</Button>
-        </div>
+        <>
+            <AppHeaderPublic />
+            <div className="background login">
+                {
+                    // {error && <b className="error">{error}</b>}
+                    // <br />
+                    // <TextField id="outlined-basic" label="Email" variant="outlined" value={email} onChange={(e) => setEmail(e.target.value)} />
+                    // <TextField id="outlined-basic" label="Password" variant="outlined" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                    // <Button variant="contained" onClick={login}>Login</Button>
+                }
+            </div>
+        </>
 
     );
 };
