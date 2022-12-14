62a84b4eb0f4bc9caf899e24fb08ac3421cadaf3
Radomir Djokovic <radomir.djokovic@yara.com>
2022-12-14 08:26:07 +0100

    Watch state update to check session

--- client_app/src/app/components/privateRoute/PrivateRoute.tsx
+++ client_app/src/app/components/privateRoute/PrivateRoute.tsx
@@ -1,8 +1,14 @@
 import { Navigate, Route } from "react-router-dom";
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
+import { useSelector } from "react-redux";
+import { getAuthUserTokenSelector } from "../../store/auth/selectors";
 
 function PrivateRoute({ children }: { children: ReactNode }) {
-  const token = localStorage.getItem("token");
+  const stateToken = useSelector(getAuthUserTokenSelector);
+  let token = localStorage.getItem("token");
+  useEffect(() => {
+    token = localStorage.getItem("token");
+  }, [stateToken]);
   if (!token) {
     return <Navigate to="/login" replace />;
   }
