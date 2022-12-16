afda883645d58cd954814fb36367a477f1b7164b
jovanadjekovic <djekovicjovana1@gmail.com>
2022-12-16 17:01:07 +0100

    login on enter click

--- client_app/src/app/components/login/Login.tsx
+++ client_app/src/app/components/login/Login.tsx
@@ -23,7 +23,14 @@ const Login = () => {
         event.preventDefault();
     };
 
-    const login = () => {
+    const handleKeyPress = (e: any) => {
+        if (e.keyCode === 13) {
+            login(e);
+        }
+    }
+
+    const login = (e: any) => {
+        e.preventDefault();
         dispatch(loginRequest({ email, password }));
     }
 
@@ -42,41 +49,43 @@ const Login = () => {
                 <Grid container className="grid-style">
                     <Grid item xs={10} sm={6} md={4} lg={4} xl={3}>
                         <Card className="login-card">
-                            {/*LABELS */}
-                            <Typography className={error ? "login-title-with-error" : "login-title"}>Sign in</Typography>
-                            {/*LABELS */}
-                            {error && <div className="login-error"><ErrorIcon className="login-error-icon" /><Typography className="login-error-message">{error}</Typography></div>
-                            }
-                            <TextField error={error ? true : false} id="outlined-basic" label="Email" variant="outlined" value={email} onChange={(e) => setEmail(e.target.value)} className="email-input" />
-                            <FormControl variant="outlined" >
+                            <form>
                                 {/*LABELS */}
-                                <InputLabel htmlFor="outlined-password" error={error ? true : false}>Password</InputLabel>
-                                <OutlinedInput
-                                    error={error ? true : false}
-                                    id="outlined-password"
-                                    type={showPassword ? 'text' : 'password'}
-                                    endAdornment={
-                                        <InputAdornment position="end">
-                                            <IconButton
-                                                aria-label="toggle password visibility"
-                                                onClick={handleClickShowPassword}
-                                                onMouseDown={handleMouseDownPassword}
-                                                edge="end"
-                                            >
-                                                {showPassword ? <VisibilityOff /> : <Visibility />}
-                                            </IconButton>
-                                        </InputAdornment>
-                                    }
-                                    label="Password"
-                                    value={password} onChange={(e) => setPassword(e.target.value)}
-                                />
-                            </FormControl>
-                            {/*LABELS */}
-                            <Typography className="login-forgot-password" onClick={forgotPasswordRedirect}>Forgot Password?</Typography>
-                            {/*LABELS */}
-                            <Button variant="contained" onClick={login} className="login-button" disableRipple> Sign in</Button>
-                            {/*LABELS */}
-                            <Typography className="login-no-account" onClick={signUpRedirect}>Don't have an account? <u>Register now</u></Typography>
+                                <Typography className={error ? "login-title-with-error" : "login-title"}>Sign in</Typography>
+                                {/*LABELS */}
+                                {error && <div className="login-error"><ErrorIcon className="login-error-icon" /><Typography className="login-error-message">{error}</Typography></div>
+                                }
+                                <TextField error={error ? true : false} id="outlined-basic" label="Email" variant="outlined" value={email} onChange={(e) => setEmail(e.target.value)} className="email-input" />
+                                <FormControl variant="outlined" >
+                                    {/*LABELS */}
+                                    <InputLabel htmlFor="outlined-password" error={error ? true : false}>Password</InputLabel>
+                                    <OutlinedInput
+                                        error={error ? true : false}
+                                        id="outlined-password"
+                                        type={showPassword ? 'text' : 'password'}
+                                        endAdornment={
+                                            <InputAdornment position="end">
+                                                <IconButton
+                                                    aria-label="toggle password visibility"
+                                                    onClick={handleClickShowPassword}
+                                                    onMouseDown={handleMouseDownPassword}
+                                                    edge="end"
+                                                >
+                                                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                                                </IconButton>
+                                            </InputAdornment>
+                                        }
+                                        label="Password"
+                                        value={password} onChange={(e) => setPassword(e.target.value)}
+                                    />
+                                </FormControl>
+                                {/*LABELS */}
+                                <Typography className="login-forgot-password" onClick={forgotPasswordRedirect}>Forgot Password?</Typography>
+                                {/*LABELS */}
+                                <Button variant="contained" onClick={login} className="login-button" disableRipple type="submit" onKeyUp={handleKeyPress}> Sign in</Button>
+                                {/*LABELS */}
+                                <Typography className="login-no-account" onClick={signUpRedirect} >Don't have an account? <u>Register now</u></Typography>
+                            </form>
                         </Card>
                     </Grid>
                 </Grid>
