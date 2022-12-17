df1619580369661cf87008dba97bd74c404d684a
jovanadjekovic <djekovicjovana1@gmail.com>
2022-12-17 11:27:24 +0100

    login- frontend validation

--- client_app/src/app/components/login/Login.tsx
+++ client_app/src/app/components/login/Login.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { loginRequest } from "../../store/auth/actions";
 import { useDispatch, useSelector } from "react-redux";
 import { getErrorSelector } from "../../store/auth/selectors";
@@ -8,14 +8,15 @@ import { Button, Card, FormControl, Grid, IconButton, InputAdornment, InputLabel
 import { Visibility, VisibilityOff } from "@mui/icons-material";
 import { useNavigate } from "react-router-dom";
 import ErrorIcon from '@mui/icons-material/Error';
+import { useForm } from "react-hook-form";
+import { getEmailRegex, getPasswordRegex } from "./../../utils/Utils";
 
 const Login = () => {
     const dispatch = useDispatch();
     const navigate = useNavigate();
     const error = useSelector(getErrorSelector);
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
     const [showPassword, setShowPassword] = React.useState(false);
+    const { register, formState: { errors }, handleSubmit } = useForm();
 
     const handleClickShowPassword = () => setShowPassword((show) => !show);
 
@@ -23,16 +24,9 @@ const Login = () => {
         event.preventDefault();
     };
 
-    const handleKeyPress = (e: any) => {
-        if (e.keyCode === 13) {
-            login(e);
-        }
-    }
-
-    const login = (e: any) => {
-        e.preventDefault();
-        dispatch(loginRequest({ email, password }));
-    }
+    const onSubmit = (data: any) => {
+        dispatch(loginRequest(data));
+    };
 
     const signUpRedirect = () => {
         navigate("/signup");
@@ -49,18 +43,26 @@ const Login = () => {
                 <Grid container className="grid-style">
                     <Grid item xs={10} sm={6} md={4} lg={4} xl={3}>
                         <Card className="login-card">
-                            <form>
+                            <form onSubmit={handleSubmit(onSubmit)}>
                                 {/*LABELS */}
-                                <Typography className={error ? "login-title-with-error" : "login-title"}>Sign in</Typography>
+                                <Typography className="login-title">Sign in</Typography>
                                 {/*LABELS */}
                                 {error && <div className="login-error"><ErrorIcon className="login-error-icon" /><Typography className="login-error-message">{error}</Typography></div>
                                 }
-                                <TextField error={error ? true : false} id="outlined-basic" label="Email" variant="outlined" value={email} onChange={(e) => setEmail(e.target.value)} className="email-input" />
+                                <TextField error={error || errors.email ? true : false}
+                                    id="outlined-basic" label="Email"
+                                    variant="outlined"
+                                    className={errors.email ? "email-input-with-error" : "email-input"}
+                                    {...register("email", { required: true, pattern: getEmailRegex() })} />
+                                {/*LABELS */}
+                                {errors.email?.type === "required" && <Typography className="login-error-fe">Email is required.</Typography>}
+                                {/*LABELS */}
+                                {errors.email?.type === "pattern" && <Typography className="login-error-fe">Email should be an email.</Typography>}
                                 <FormControl variant="outlined" >
                                     {/*LABELS */}
-                                    <InputLabel htmlFor="outlined-password" error={error ? true : false}>Password</InputLabel>
+                                    <InputLabel htmlFor="outlined-password">Password</InputLabel>
                                     <OutlinedInput
-                                        error={error ? true : false}
+                                        error={error || errors.password ? true : false}
                                         id="outlined-password"
                                         type={showPassword ? 'text' : 'password'}
                                         endAdornment={
@@ -76,13 +78,17 @@ const Login = () => {
                                             </InputAdornment>
                                         }
                                         label="Password"
-                                        value={password} onChange={(e) => setPassword(e.target.value)}
+                                        {...register("password", { required: true, pattern: getPasswordRegex() })}
                                     />
+                                    {/*LABELS */}
+                                    {errors.password?.type === "required" && <Typography className="login-error-fe">Password is required.</Typography>}
+                                    {/*LABELS */}
+                                    {errors.password?.type === "pattern" && <Typography className="login-error-fe">Password must contain at least 8 characters and at least 1 lowerace letter, 1 uppercase letter and 1 number.</Typography>}
                                 </FormControl>
                                 {/*LABELS */}
                                 <Typography className="login-forgot-password" onClick={forgotPasswordRedirect}>Forgot Password?</Typography>
                                 {/*LABELS */}
-                                <Button variant="contained" onClick={login} className="login-button" disableRipple type="submit" onKeyUp={handleKeyPress}> Sign in</Button>
+                                <Button variant="contained" className="login-button" disableRipple type="submit" > Sign in</Button>
                                 {/*LABELS */}
                                 <Typography className="login-no-account" onClick={signUpRedirect} >Don't have an account? <u>Register now</u></Typography>
                             </form>
