cff75b6fdb2ba05221a518a5ea55fd15e239ccf2
jovanadjekovic <djekovicjovana1@gmail.com>
2022-12-27 14:20:45 +0100

    get self na private route

--- client_app/src/app/components/home/Home.tsx
+++ client_app/src/app/components/home/Home.tsx
@@ -1,17 +1,8 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { getSelfRequest, logout } from "../../store/auth/actions";
-import { getAuthUserSelector } from "../../store/auth/selectors";
+import React from "react";
 import AppFooter from "../app-footer/AppFooter";
 import AppHeaderPrivate from "../app-header-private/AppHeaderPrivate";
 
 const Home = () => {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(getSelfRequest());
-  }, [dispatch]);
-
   return (
     <>
       <AppHeaderPrivate />
--- client_app/src/app/components/privateRoute/PrivateRoute.tsx
+++ client_app/src/app/components/privateRoute/PrivateRoute.tsx
@@ -1,11 +1,18 @@
 import { Navigate, Route } from "react-router-dom";
 import { ReactNode, useEffect } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { getAuthUserTokenSelector } from "../../store/auth/selectors";
+import { getSelfRequest } from "../../store/auth/actions";
 
 function PrivateRoute({ children }: { children: ReactNode }) {
   const stateToken = useSelector(getAuthUserTokenSelector);
   let token = localStorage.getItem("token");
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getSelfRequest());
+  }, [dispatch]);
+
   useEffect(() => {
     token = localStorage.getItem("token");
   }, [stateToken]);
