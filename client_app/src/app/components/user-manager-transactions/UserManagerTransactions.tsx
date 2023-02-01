4f542af5b1f7f9d421a53997738703afab1b4209
jovanadjekovic <djekovicjovana1@gmail.com>
2023-02-01 10:13:48 +0100

    small

--- client_app/src/app/components/user-manager-transactions/UserManagerTransactions.tsx
+++ client_app/src/app/components/user-manager-transactions/UserManagerTransactions.tsx
@@ -1,8 +1,12 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { getAuthUserSelector } from "../../store/auth/selectors";
 import AppFooter from "../app-footer/AppFooter";
 import AppHeaderPrivate from "../app-header-private/AppHeaderPrivate";
 
 const UserManagerTransactions = () => {
+  const user = useSelector(getAuthUserSelector);
+
   return (
     <>
       <AppHeaderPrivate />
