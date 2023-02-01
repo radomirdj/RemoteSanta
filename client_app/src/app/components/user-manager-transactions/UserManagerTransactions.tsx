bfe812788aef3119d59f4d12b643bcec6b6cc91a
jovanadjekovic <djekovicjovana1@gmail.com>
2023-02-01 11:28:02 +0100

    user manager transactions

--- client_app/src/app/components/user-manager-transactions/UserManagerTransactions.tsx
+++ client_app/src/app/components/user-manager-transactions/UserManagerTransactions.tsx
@@ -1,16 +1,139 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { getAuthUserSelector } from "../../store/auth/selectors";
+import {
+  fetchOrganization,
+  fetchOrganizationTransactionList,
+} from "../../store/orgs/actions";
+import {
+  getOrganizationSelector,
+  getOrganizationTransactionListSelector,
+} from "../../store/orgs/selectors";
 import AppFooter from "../app-footer/AppFooter";
 import AppHeaderPrivate from "../app-header-private/AppHeaderPrivate";
+import {
+  DataGrid,
+  GridColDef,
+  GridRowsProp,
+  gridClasses,
+} from "@mui/x-data-grid";
+import styled from "@emotion/styled";
+import { Box, Grid, Typography } from "@mui/material";
+import ToolbarQuickFilter from "../ToolbarQuickFilter/ToolbarQuickFilter";
+import CustomPagination from "../custom-pagination/CustomPagination";
 
 const UserManagerTransactions = () => {
   const user = useSelector(getAuthUserSelector);
+  const dispatch = useDispatch();
+  const organization = useSelector(getOrganizationSelector);
+  const organizationTransactionList = useSelector(
+    getOrganizationTransactionListSelector
+  );
+  const rowsPerPage = 7;
+
+  useEffect(() => {
+    dispatch(fetchOrganization());
+    dispatch(fetchOrganizationTransactionList());
+  }, [dispatch]);
+
+  const columns: GridColDef[] = [
+    { field: "type", headerName: "Type", width: 300 },
+    { field: "createdAt", headerName: "Created At", width: 200 },
+    { field: "event", headerName: "Event", width: 300 },
+    { field: "amount", headerName: "Amount", width: 200 },
+  ];
+
+  const rows: GridRowsProp = organizationTransactionList.map(
+    (organizationTransaction) => {
+      return {
+        type: organizationTransaction.type,
+        createdAt: new Date(organizationTransaction.createdAt)
+          .toLocaleDateString()
+          .replaceAll("/", "."),
+        event:
+          organizationTransaction.event?.title &&
+          organizationTransaction.event?.title
+            ? organizationTransaction.event?.title +
+              " - validTo: " +
+              new Date(
+                organizationTransaction.event?.validTo
+                  ? organizationTransaction.event?.validTo
+                  : ""
+              )
+                .toLocaleDateString()
+                .replaceAll("/", ".")
+            : "",
+        amount: organizationTransaction.totalAmount + " PTS",
+        id: organizationTransaction.id,
+      };
+    }
+  );
+
+  const StripedDataGrid = styled(DataGrid)(({ theme }) => ({
+    [`& .${gridClasses.row}.even`]: {
+      backgroundColor: "#EDEEC4",
+      "&:hover:none, &.Mui-hovered": {
+        "@media (hover: none)": {
+          backgroundColor: "#EDEEC4",
+        },
+      },
+    },
+    [`& .${gridClasses.row}.odd`]: {
+      backgroundColor: "#ffffff",
+      "&:hover:none, &.Mui-hovered": {
+        "@media (hover: none)": {
+          backgroundColor: "#ffffff",
+        },
+      },
+    },
+  }));
 
   return (
     <>
       <AppHeaderPrivate />
-      <div className="background user-manager-transactions"></div>
+      <div className="background user-manager-transactions">
+        <Grid container className="grid-style">
+          <Grid item xs={6}>
+            <Typography className="organization-title">
+              {organization?.name}
+            </Typography>
+          </Grid>
+          <Grid item xs={6}>
+            <Typography className="balance-title">
+              Balance - {organization?.balance} PTS
+            </Typography>
+          </Grid>
+          <Box className="box-style">
+            <StripedDataGrid
+              rows={rows}
+              pagination
+              pageSize={rowsPerPage}
+              rowsPerPageOptions={[rowsPerPage]}
+              columns={columns}
+              getRowId={(row: any) => row.id}
+              disableDensitySelector
+              disableColumnSelector
+              disableColumnFilter
+              disableColumnMenu
+              disableSelectionOnClick
+              components={{
+                Toolbar: ToolbarQuickFilter,
+                Pagination: CustomPagination,
+              }}
+              componentsProps={{
+                toolbar: {
+                  showQuickFilter: true,
+                  quickFilterProps: { debounceMs: 500 },
+                },
+              }}
+              className="data-grid"
+              getRowClassName={(params) =>
+                params.indexRelativeToCurrentPage % 2 === 0 ? "even" : "odd"
+              }
+            />
+          </Box>
+        </Grid>
+      </div>
       <AppFooter />
     </>
   );
