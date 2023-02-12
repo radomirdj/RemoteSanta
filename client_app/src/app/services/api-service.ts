70ddc311761c1e5c4faa80a0691b667cca9124ee
Radomir Djokovic <radomir.djokovic@yara.com>
2023-02-12 13:12:44 +0100

    Set default BE URL for prod

--- client_app/src/app/services/api-service.ts
+++ client_app/src/app/services/api-service.ts
@@ -32,12 +32,12 @@ import {
   PostUserInvitePayload
 } from "../store/user-invites/types";
 
-console.log(
-  "-------process.env.REACT_APP_BASE_URL",
-  process.env.REACT_APP_BASE_URL
-);
+const baseURL =
+  process.env.REACT_APP_BASE_URL ||
+  "http://ec2-3-145-156-72.us-east-2.compute.amazonaws.com/api/";
+
 const api = axios.create({
-  baseURL: `${process.env.REACT_APP_BASE_URL}api`
+  baseURL
 });
 
 export const getAdminOrganizationTransactionList = (
