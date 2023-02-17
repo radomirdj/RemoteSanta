afdc1ed6a5b54cb00bcab7b5f8e37e7dd08657e6
jovanadjekovic <djekovicjovana1@gmail.com>
2023-02-17 20:57:07 +0100

    video player

--- client_app/src/app/components/demo/Demo.tsx
+++ client_app/src/app/components/demo/Demo.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import AppFooter from "../app-footer/AppFooter";
 import AppHeaderPrivate from "../app-header-private/AppHeaderPrivate";
+import ReactPlayer from "react-player";
 
 const Demo = () => {
   return (
     <>
       <AppHeaderPrivate />
-      <div className="background demo"></div>
+      <div className="background demo">
+        <ReactPlayer
+          url="https://www.youtube.com/watch?v=iGgHvzxmR8Q"
+          controls={true}
+        />
+      </div>
       <AppFooter />
     </>
   );
