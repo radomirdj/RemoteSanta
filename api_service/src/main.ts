81c185dc1a90d587c2190e0fe640de7f7cc7fea4
Radomir Djokovic <radomir.djokovic@yara.com>
2023-02-20 13:49:13 +0100

    add auto_prod

--- api_service/src/main.ts
+++ api_service/src/main.ts
@@ -5,16 +5,19 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const config = new DocumentBuilder()
-    .setTitle('Median')
-    .setDescription('The Median API description')
-    .setVersion('0.1')
-    .build();
+  if (process.env.NODE_ENV === 'development') {
+    const config = new DocumentBuilder()
+      .setTitle('Median')
+      .setDescription('The Median API description')
+      .setVersion('0.1')
+      .build();
 
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
 
-  if (process.env.ENABLE_ALL_CORS) app.enableCors();
+    app.enableCors();
+  }
+
   await app.listen(process.env.PORT);
   console.log(
     'bootstrap -> process.env.PORT',
